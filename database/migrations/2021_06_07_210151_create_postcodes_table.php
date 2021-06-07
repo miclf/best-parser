2d079aef2d052fc29e4c24163a18cb367ede4690
michael <michael@estsurinter.net>
2021-06-07 23:08:07 +0200

    Add migration for `postcodes` table

---
+++ database/migrations/2021_06_07_210151_create_postcodes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostcodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('postcodes', function (Blueprint $table) {
+
+            $table->unsignedSmallInteger('id')->primary();
+            $table->string('namespace');
+            $table->string('version');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('postcodes');
+    }
+}

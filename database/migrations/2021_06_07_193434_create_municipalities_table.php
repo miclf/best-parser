8d848b31ef4e29fd29b5ed92fd7c2fa134bed1e0
michael <michael@estsurinter.net>
2021-06-07 22:51:49 +0200

    Add migration for `municipalities` table

---
+++ database/migrations/2021_06_07_193434_create_municipalities_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMunicipalitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('municipalities', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('id')->primary();
+            $table->string('name_fr')->nullable();
+            $table->string('name_nl')->nullable();
+            $table->string('name_de')->nullable();
+            $table->unsignedMediumInteger('nis_code')->index();
+            $table->string('namespace');
+            $table->string('version');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('municipalities');
+    }
+}
